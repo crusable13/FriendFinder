d407baa6838aa5025f6b2f118a522e2286005748
Christopher <jcelitesmith@gmail.com>
2019-05-07 11:14:44 -0700

    Fully functional

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
 
-module.exports =  function (app) {
-        app.get("/survey", function (req, res) {
-            res.sendFile(path.join(__dirname,  "../public/survey.html"))
-        });
+// ===============================================================================
+// ROUTING
+// ===============================================================================
 
-        app.get("/", function (req, res) {
-            res.sendFile(path.join(__dirname, "../public/home.html"))
-        });
+module.exports = function(app) {
 
-        app.get('*', function (req, res) {
-            res.redirect('/');
-        });
-    }
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
 
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+
+};
